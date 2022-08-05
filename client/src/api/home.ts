65b035c410ffab27f53ce124fca033ad9324ec3e
Huy Hoang <hoanghuyvu2k@gmail.com>
2022-08-05 10:18:58 +0700

    add ts

--- client/src/api/home.ts
+++ client/src/api/home.ts
@@ -1,12 +1,12 @@
 import { ApiClient } from "./config";
 
 const homeApi = {
-    getListElectronic: (payload) => {
-        return ApiClient.post('electronic-items',payload);
-    },
-    getListBook: (payload) => {
-        return ApiClient.post('book-search', payload);
-    }
+  // getListElectronic: (payload) => {
+  //     return ApiClient.post('electronic-items',payload);
+  // },
+  // getListBook: (payload) => {
+  //     return ApiClient.post('book-search', payload);
+  // }
 };
 
-export {homeApi};
+export { homeApi };
