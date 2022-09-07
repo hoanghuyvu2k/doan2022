fee9e8a214a67577ca721216bbef67c3a60006a7
Hoang Huy <hoangnx@mor.com.vn>
2022-09-07 08:52:21 +0700

    add detele post

--- client/src/pages/admin/Content.jsx
+++ client/src/pages/admin/Content.jsx
@@ -1,23 +1,37 @@
 import { CCard, CCardHeader, CCardBody } from "@coreui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
-function ListContent() {
+function ListContent(props) {
   const [posts, setPosts] = useState([]);
+  const history = useHistory();
   useEffect(async () => {
     const res = await axios.get("/posts" + "?user=admin");
     setPosts(res.data);
     console.log(posts);
   }, []);
+  const handlePush = (id) => {
+    history.push("/post/" + id);
+  };
   return (
-    <div>
+    <div className="list-content">
+      <h1>Danh sách bài viết</h1>
       {posts.map((post) => {
         return (
           <CCard className="mb-4">
-            <CCardHeader>{post.title}</CCardHeader>{" "}
-            <CCardBody className="d-flex justify-content-between">
+            <CCardHeader>{post.title}</CCardHeader>
+            <CCardBody className="d-flex justify-content-between position-relative">
               <div>{post.desc}</div>
-              <div>Show more content</div>
+              <div className="show-more">Show more content</div>
+              <i
+                className="icon-edit far fa-edit"
+                onClick={() => handlePush(post._id)}
+              ></i>
+              <i
+                className="icon-delete far fa-trash-alt"
+                // onClick={handleDelete}
+              ></i>
             </CCardBody>
           </CCard>
         );
